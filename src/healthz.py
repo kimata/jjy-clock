551f554d667269d4aad53c0153bb87ed6e1cff5b
KIMATA Tetsuya <kimata@green-rabbit.net>
2025-05-11 18:26:10 +0900

    fix: オプションの指定方法を共通化

--- src/healthz.py
+++ src/healthz.py
@@ -3,11 +3,11 @@
 Liveness のチェックを行います
 
 Usage:
-  healthz.py [-c CONFIG] [-d]
+  healthz.py [-c CONFIG] [-D]
 
 Options:
   -c CONFIG         : CONFIG を設定ファイルとして読み込んで実行します．[default: config.yaml]
-  -d                : デバッグモードで動作します．
+  -D                : デバッグモードで動作します．
 """
 
 import logging
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     args = docopt(__doc__)
 
     config_file = args["-c"]
-    debug_mode = args["-d"]
+    debug_mode = args["-D"]
 
     my_lib.logger.init("hems.rasp-aqua", level=logging.DEBUG if debug_mode else logging.INFO)
 
